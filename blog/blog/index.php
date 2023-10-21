80cad17f4672b166a8d9f88f84c616b183fa7ddc
Gabriel Arnaud <gabriel.arnaud30@gmail.com>
2023-10-21 15:43:42 -0300

    aula 31

--- blog/blog/index.php
+++ blog/blog/index.php
@@ -4,8 +4,4 @@
     require_once 'sistema/configuracao.php';
     include_once 'Helpers.php';
 
-    $url = 'http://unset.com';
-
-    var_dump(validarUrl($url));
-    echo '<br>';
-    var_dump(validarUrlComFiltro($url));
+    echo SITE_NOME;
