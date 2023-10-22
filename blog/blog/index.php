33f268bac2771a69c129f0251ed21fb26b62f62b
Gabriel Arnaud <gabriel.arnaud30@gmail.com>
2023-10-22 02:12:27 -0300

    Até a aula 50
    Instalando o composer, aprendendo a pegar uma biblioteca e a retirar uma biblioteca.

--- blog/blog/index.php
+++ blog/blog/index.php
@@ -1,15 +1,11 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <?php
-//Arquivo index responsável pela inicialização do sistema
-require_once 'sistema/configuracao.php';
-include_once './sistema/Nucleo/Helpers.php';
-include './sistema/Nucleo/Mensagem.php';
-include './sistema/Nucleo/Controlador.php';
-
+//Arquivo index responsável pela inicialização do sistem
+require 'vendor/autoload.php';
 
 use sistema\Nucleo\Helpers;
 use sistema\Nucleo\Controlador;
 
 
-$controlador = new Controlador('admin');
-var_dump($controlador);
+$document = new \Bissolli\ValidadorCpfCnpj\CPF('018.287.264-51');
+
+var_dump($document->isValid());
