5771f06d9aff6e032ddf37dd934d5d6d87601f38
Gabriel Arnaud <gabriel.arnaud30@gmail.com>
2023-10-21 23:26:49 -0300

    Aula 42
    Aprendendo sobre encadeamento de metodos e inicio de bootstrap.

--- blog/blog/index.php
+++ blog/blog/index.php
@@ -1,13 +1,13 @@
-    <?php
-    //Arquivo index responsável pela inicialização do sistema
-
-    require_once 'sistema/configuracao.php';
-    include_once 'Helpers.php';
-    include './sistema/Nucleo/Mensagem.php';
-
-    $msg = new Mensagem();
-    echo '<hr>';
-    echo $msg->renderizar();
-    echo '<hr>';
-
-    var_dump($msg);
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<?php
+//Arquivo index responsável pela inicialização do sistema
+require_once 'sistema/configuracao.php';
+include_once 'Helpers.php';
+include './sistema/Nucleo/Mensagem.php';
+$msg = new Mensagem();
+echo '<hr>';
+echo $msg->sucesso('Mensagem de sucesso')->renderizar();
+echo '<hr>';
+echo $msg->erro('Mensagem de erro')->renderizar();
+echo '<hr>';
+var_dump($msg);
