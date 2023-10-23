7fe8c22429346bcb71b6af12d2efe350616b5b8e
Gabriel Arnaud <gabriel.arnaud30@gmail.com>
2023-10-22 23:34:06 -0300

    aula 51
    Aprendendo sobre htaccess

--- blog/blog/index.php
+++ blog/blog/index.php
@@ -5,7 +5,4 @@ require 'vendor/autoload.php';
 use sistema\Nucleo\Helpers;
 use sistema\Nucleo\Controlador;
 
-
-$document = new \Bissolli\ValidadorCpfCnpj\CPF('018.287.264-51');
-
-var_dump($document->isValid());
+echo 'test';
