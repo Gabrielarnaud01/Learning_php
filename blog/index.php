ac1e919282beb1420e4c5b2b6a397d4e63d88bb9
Gabriel Arnaud <gabriel.arnaud30@gmail.com>
2023-10-25 19:54:29 -0300

    aprendendo try,catch finally, aula 64

--- blog/index.php
+++ blog/index.php
@@ -1,8 +1,17 @@
 <?php
 //Arquivo index responsável pela inicialização do sistem
-require 'vendor/autoload.php';
-require 'rotas.php';
-
 
 use sistema\Nucleo\Helpers;
 use sistema\Nucleo\Controlador;
+
+
+require 'vendor/autoload.php';
+//require 'rotas.php';
+
+try {
+    Helpers::validarCpf('12312312312');
+} catch (Exception $e) {
+    echo $e->getMessage();
+} finally {
+    echo 'oi';
+}
