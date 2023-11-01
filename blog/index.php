5fab156c520bb1e4c40a54b65d4740f3cb8e0fd4
Gabriel Arnaud <gabriel.arnaud30@gmail.com>
2023-10-31 22:56:23 -0300

    aula 71

--- blog/index.php
+++ blog/index.php
@@ -6,7 +6,11 @@ require 'vendor/autoload.php';
 //require 'rotas.php';
 use sistema\Modelo\PostModelo;
 
-$posts = (new PostModelo())->ler();
+
+
+
+$posts = (new PostModelo())->ler(3);
+
 
 foreach ($posts as $post) {
     echo $post->titulo . "<br>";
--- blog/sistema/Modelo/PostModelo.php
+++ blog/sistema/Modelo/PostModelo.php
@@ -6,9 +6,14 @@ use sistema\Nucleo\Conexao;
 
 class PostModelo
 {
-    public function ler(): array
+    public function ler(int $id = null): array
     {
-        $query = "SELECT * FROM posts ";
+        $where = ($id ? "WHERE id = {$id}" : '');
+
+        $query = "SELECT * FROM posts {$where} ";
+        //$query = "SELECT titulo,texto FROM posts "; voce pode escolher qual dado vai pegar.
+        //$query = "SELECT * FROM posts WHERE id=3 AND status = 1 OR status = 0";
+        //$query = "SELECT * FROM posts LIMIT 2 OFFSET 2";
         $stmt =  Conexao::getInstancia()->query($query);
         $resultado = $stmt->fetchAll();
 
